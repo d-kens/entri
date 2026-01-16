cdf90f6a0246c6d2d92db1add6c28004d173d461
d-kens <omondikens1@gmail.com>
2026-01-16 21:33:43 +0300

    feat(auth): Implement login endpoint

--- api/src/main/java/com/parrcel/api/modules/auth/dto/AuthResponse.java
+++ api/src/main/java/com/parrcel/api/modules/auth/dto/AuthResponse.java
@@ -1,4 +1,11 @@
 package com.parrcel.api.modules.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
 }
