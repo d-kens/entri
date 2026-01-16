f24fb2d7121b4599a37491570cba1fd5c2c2edb5
d-kens <omondikens1@gmail.com>
2026-01-16 21:33:43 +0300

    feat(auth): Implement login endpoint

--- api/src/main/java/com/parrcel/api/modules/auth/dto/AccessToken.java
+++ api/src/main/java/com/parrcel/api/modules/auth/dto/AccessToken.java
@@ -1,4 +1,10 @@
 package com.parrcel.api.modules.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AccessToken {
+    private String accessToken;
 }
