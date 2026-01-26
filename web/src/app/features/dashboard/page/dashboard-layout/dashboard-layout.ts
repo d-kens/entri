fb374c75cc522173cb90c5014b7329a5cd1c1dd6
d-kens <omondikens1@gmail.com>
2026-01-26 17:59:13 +0300

    Initial commit

--- web/src/app/features/dashboard/page/dashboard-layout/dashboard-layout.ts
+++ web/src/app/features/dashboard/page/dashboard-layout/dashboard-layout.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule
+  ],
   templateUrl: './dashboard-layout.html',
   styleUrl: './dashboard-layout.css',
 })
