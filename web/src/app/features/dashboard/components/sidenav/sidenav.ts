e92350fb962e8b2d6bf357f6c58367be9bb7b90e
d-kens <omondikens1@gmail.com>
2026-01-27 15:46:52 +0300

    feat/dashboard: Implement sidenav UI, header and sidenav links

--- web/src/app/features/dashboard/components/sidenav/sidenav.ts
+++ web/src/app/features/dashboard/components/sidenav/sidenav.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+
+
+export type MenuItem = {
+  icon: string,
+  label: string,
+  route?: string
+}
 
 @Component({
   selector: 'app-sidenav',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatIconModule
+  ],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.css',
 })
 export class Sidenav {
+  menuItem = signal<MenuItem[]>([
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard'
+    },
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard'
+    },
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard'
+    },
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard'
+    },
+    {
+      icon: 'dashboard',
+      label: 'Dashboard',
+      route: 'dashboard'
+    }
+  ])
 
 }
