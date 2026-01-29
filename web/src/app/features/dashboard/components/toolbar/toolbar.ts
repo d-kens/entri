bfebf69fcdcb5723b96ee7cb100036de8645c943
d-kens <omondikens1@gmail.com>
2026-01-29 22:11:19 +0300

    Implement functionality to load current logged in user details and display in the user details section in the tool bar

--- web/src/app/features/dashboard/components/toolbar/toolbar.ts
+++ web/src/app/features/dashboard/components/toolbar/toolbar.ts
@@ -1,10 +1,10 @@
-import { Component, input, output } from '@angular/core';
+import {Component, inject, input, output, signal, OnInit} from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {UserProfile} from '@features/dashboard/components/user-profile/user-profile';
-
-type UserRole = 'ADMIN' | 'MERCHANT' | 'AGENT' | 'CUSTOMER';
+import {UserResponse} from '@core/models/user.models';
+import {Auth} from '@core/services/auth';
 
 @Component({
   selector: 'app-toolbar',
@@ -18,15 +18,32 @@ type UserRole = 'ADMIN' | 'MERCHANT' | 'AGENT' | 'CUSTOMER';
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.css',
 })
-export class Toolbar {
+export class Toolbar implements OnInit {
+
+  private authService = inject(Auth);
+
   isMobile = input<boolean>(false);
-  userName = input<string>('');
-  walletBalance = input<number>(0);
-  userRole = input<UserRole>('MERCHANT');
+  walletBalance = signal(12450.50);
+  user = signal<UserResponse | undefined>(undefined);
 
   toggleSidenav = output<void>();
 
+  ngOnInit() {
+    this.getCurrentUser();
+  }
+
   onToggleSidenav() {
     this.toggleSidenav.emit();
+  }
+
+  getCurrentUser() {
+    this.authService.getCurrentUser().subscribe({
+      next: (user: UserResponse) => {
+        this.user.set(user);
+      },
+      error: (error) => {
+        console.error('Failed to fetch user:', error);
+      }
+    });
   }
 }
