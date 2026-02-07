f8dac228cb801ab47ec2296ea6c9aa204c45cd28
d-kens <omondikens1@gmail.com>
2026-02-07 11:26:19 +0300

    Implement the UI for creating a new delivery request

--- web/src/app/features/feature.routes.ts
+++ web/src/app/features/feature.routes.ts
@@ -1,11 +1,19 @@
 import {Routes} from '@angular/router';
 import {Layout} from '@features/layout/layout';
+import {Welcome} from '@features/layout/components/welcome/welcome';
+
+// TODO: Fix logout issue
+// TODO: Fix User Profile Card Issues
 
 export const FEATURE_ROUTES: Routes = [
   {
     path: '',
     component: Layout,
     children: [
+      {
+        path: '',
+        component: Welcome
+      },
       {
         path: 'deliveries',
         loadChildren: () => import('./deliveries/deliveries.routes').then(m => m.DELIVERIES_ROUTES)
