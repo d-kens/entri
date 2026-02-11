6980605239a4b7940f6ea1421bdd6b4545c34bd5
d-kens <omondikens1@gmail.com>
2026-02-11 19:53:50 +0300

    Implement calculate delivery fee function

--- api/src/main/java/com/parrcel/api/modules/deliveries/service/DeliveryService.java
+++ api/src/main/java/com/parrcel/api/modules/deliveries/service/DeliveryService.java
@@ -1,7 +1,5 @@
 package com.parrcel.api.modules.deliveries.service;
 
-import com.parrcel.api.modules.deliveries.dto.CalcDeliveryFeeDTO;
-import com.parrcel.api.modules.deliveries.dto.DeliveryFeeDTO;
 import com.parrcel.api.modules.zones.service.AgentService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,14 +8,4 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class DeliveryService {
     private final AgentService agentService;
-
-    public DeliveryFeeDTO calculateDeliveryFee(CalcDeliveryFeeDTO request) {
-        var fromPoint = agentService.getAgentPointById(request.fromAgent());
-        var toPoint = agentService.getAgentPointById(request.toAgent());
-
-        if (fromPoint.getZone().getIsCbd() || toPoint.getZone().getIsCbd())
-            return new DeliveryFeeDTO(150.0);
-        else
-            return new DeliveryFeeDTO(200.0);
-    }
 }
