3f8c883a0cb6b033c26dc6dd4bf0739b8978295d
d-kens <omondikens1@gmail.com>
2026-02-12 22:25:49 +0300

    Implement delivery request validation

--- api/src/main/java/com/parrcel/api/modules/deliveries/controller/DeliveryController.java
+++ api/src/main/java/com/parrcel/api/modules/deliveries/controller/DeliveryController.java
@@ -1,9 +1,16 @@
 package com.parrcel.api.modules.deliveries.controller;
 
+import com.parrcel.api.modules.deliveries.dto.CreateDeliveryDto;
+import com.parrcel.api.modules.deliveries.dto.DeliveryResponseDto;
 import com.parrcel.api.modules.deliveries.service.DeliveryService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 @RestController
@@ -12,4 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeliveryController {
 
     private final DeliveryService deliveryService;
+
+    @PostMapping
+    private ResponseEntity<DeliveryResponseDto> create(
+            UriComponentsBuilder componentsBuilder,
+            @Valid @RequestBody CreateDeliveryDto createDeliveryDto
+    ) {
+        return ResponseEntity.ok(new DeliveryResponseDto());
+    }
 }
