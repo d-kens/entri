c910b1456274dbf31c624706f90f06457516ab87
d-kens <omondikens1@gmail.com>
2026-03-02 08:16:55 +0300

    Add endpoint to test mpesa stk callback

--- api/src/main/java/com/parrcel/api/modules/payment/controller/PaymentController.java
+++ api/src/main/java/com/parrcel/api/modules/payment/controller/PaymentController.java
@@ -2,15 +2,20 @@ package com.parrcel.api.modules.payment.controller;
 
 
 import com.parrcel.api.modules.payment.dto.PaymentResponse;
+import com.parrcel.api.modules.payment.providers.dto.mpesa.StkCallbackDto;
 import com.parrcel.api.modules.payment.service.PaymentService;
 import com.parrcel.api.modules.payment.dto.InitiatePaymentDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @RestController
 @RequestMapping("payments")
 @RequiredArgsConstructor
@@ -25,4 +30,11 @@ public class PaymentController {
         return paymentService.initiatePayment(dto);
     }
 
+    @PostMapping("/mpesa/callback")
+    public ResponseEntity<Void> mpesaStkCallback(
+            @RequestBody StkCallbackDto stkCallbackDto
+    ) {
+        log.info("Received M-Pesa STK callback: {}", stkCallbackDto);
+        return ResponseEntity.ok().build();
+    }
 }
