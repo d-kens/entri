c8ef8ccab9449c6a9e3f4b9b0bc79399316c729e
d-kens <omondikens1@gmail.com>
2026-03-05 14:54:41 +0300

    Log delivery payment status after marking it as paid

--- api/src/main/java/com/parrcel/api/modules/deliveries/service/DeliveryService.java
+++ api/src/main/java/com/parrcel/api/modules/deliveries/service/DeliveryService.java
@@ -106,7 +106,16 @@ public class DeliveryService {
                 .orElseThrow(() -> new NotFoundException("Delivery not found: " + deliveryExternalId));
 
         delivery.markAsPaid();
+
+        System.out.println("------------------------------------------");
+        System.out.println(delivery.getPaymentStatus());
+        System.out.println("------------------------------------------");
+
         deliveryRepository.save(delivery);
+
+        System.out.println("------------------------------------------");
+        System.out.println(delivery.getPaymentStatus());
+        System.out.println("------------------------------------------");
 
         log.info("Delivery {} marked as PAID", deliveryExternalId);
     }
--- api/src/main/java/com/parrcel/api/modules/payment/enums/PaymentStatus.java
+++ api/src/main/java/com/parrcel/api/modules/payment/enums/PaymentStatus.java
@@ -3,15 +3,5 @@ package com.parrcel.api.modules.payment.enums;
 public enum PaymentStatus {
     PENDING,
     PAID,
-    FAILED,
-    REFUNDED;
-
-    public boolean isSuccessful() {
-        return this == PAID;
-    }
-
-    public boolean canBeRefunded() {
-        return this == PAID;
-    }
+    FAILED
 }
-
