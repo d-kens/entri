fd8ac87b815deefd4a875e6376c30e57a758f511
d-kens <omondikens1@gmail.com>
2026-05-11 21:29:17 +0300

    Add password reset component and check password reset token availability in the url before rendering the token

--- web/src/app/features/auth/auth.routes.ts
+++ web/src/app/features/auth/auth.routes.ts
@@ -4,6 +4,7 @@ import {Login} from './components/login/login';
 import {Register} from './components/register/register';
 import {noAuthGuard} from '@core/guards/no-auth-guard';
 import {ForgotPassword} from '@features/auth/components/forgot-password/forgot-password';
+import {ResetPassword} from '@features/auth/components/reset-password/reset-password';
 
 export const AUTH_ROUTES: Routes = [
   {
@@ -14,7 +15,8 @@ export const AUTH_ROUTES: Routes = [
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', component: Login },
       { path: 'register', component: Register },
-      { path: 'forgot-password',component: ForgotPassword }
+      { path: 'forgot-password',component: ForgotPassword },
+      { path: 'reset-password',component: ResetPassword }
     ]
   }
 ]
