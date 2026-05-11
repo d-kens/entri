7176d6b86d32bc9c63b8aad2091e3572dad7f444
d-kens <omondikens1@gmail.com>
2026-05-11 21:18:03 +0300

    refactor: update password reset type

--- web/src/app/core/models/auth.models.ts
+++ web/src/app/core/models/auth.models.ts
@@ -25,7 +25,6 @@ export type AccessTokenResponse = {
 }
 
 export type ResetPasswordRequest = {
-  phoneNumber: string,
-  otp: string,
-  newPassword: string
+  token: string,
+  password: string
 }
