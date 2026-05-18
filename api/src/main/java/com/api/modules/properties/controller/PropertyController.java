71d52d1866a9513a291d1ec9965f1c676f541972
d-kens <omondikens1@gmail.com>
2026-05-18 21:03:15 +0300

    stub create property functionality

--- api/src/main/java/com/api/modules/properties/controller/PropertyController.java
+++ api/src/main/java/com/api/modules/properties/controller/PropertyController.java
@@ -1,7 +1,12 @@
 package com.api.modules.properties.controller;
 
+import com.api.modules.properties.dto.CreatePropertyRequest;
+import com.api.modules.properties.entity.Property;
 import com.api.modules.properties.service.PropertyService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/properties")
 public class PropertyController {
     private final PropertyService propertyService;
+
+    @PostMapping
+    public Property createProperty(
+            @Valid @RequestBody CreatePropertyRequest request
+    ) {
+        return createProperty(request);
+    }
 }
--- api/src/main/java/com/api/modules/properties/dto/CreatePropertyRequest.java
+++ api/src/main/java/com/api/modules/properties/dto/CreatePropertyRequest.java
@@ -1,18 +1,34 @@
 package com.api.modules.properties.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.math.BigDecimal;
 
 public record CreatePropertyRequest(
+        @NotBlank(message = "Property name is required")
         String propertyName,
+        @NotBlank(message = "Description is required")
         String description,
+        @NotNull(message = "Rent due date is required")
+        @Min(value = 1, message = "Rent due date must be between 1 and 31")
+        @Max(value = 31, message = "Rent due date must be between 1 and 31")
         Integer rentDueDate,
         Integer gracePeriodDate,
+        @NotBlank(message ="Country is required")
         String country,
+        @NotBlank(message ="City is required")
         String city,
+        @NotBlank(message ="Area is required")
         String area,
-        String address,
+        @NotNull(message = "Latitude is required")
         BigDecimal latitude,
+        @NotNull(message = "Longitude is required")
         BigDecimal longitude,
-        String coverImageUrl,
+        @NotNull(message = "Cover image is required")
+        MultipartFile coverImage,
         Boolean isListed
 ) {}
