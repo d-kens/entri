660908ed8848e0782f580cfdab3e0a312002a1c8
d-kens <omondikens1@gmail.com>
2026-06-24 23:25:15 +0300

    Create a failing createTicketType_eventNotFound_throwsNotFoundException test

--- api/src/main/java/com/entri/modules/events/service/TicketTypeService.java
+++ api/src/main/java/com/entri/modules/events/service/TicketTypeService.java
@@ -1,23 +1,16 @@
 package com.entri.modules.events.service;
 
-import com.entri.common.exception.NotFoundException;
 import com.entri.modules.events.dto.CreateTicketTypeRequest;
 import com.entri.modules.events.dto.TicketTypeResponse;
-import com.entri.modules.events.repository.EventRepository;
-import com.entri.modules.events.repository.TicketTypeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TicketTypeService {
-    private final EventRepository eventRepository;
-    private final TicketTypeRepository ticketTypeRepository;
 
-    public TicketTypeResponse createTicketType(final String eventExternalId, CreateTicketTypeRequest ticketTypeRequest) {
-        var event = eventRepository.findByExternalId(eventExternalId)
-                .orElseThrow(() -> new NotFoundException("Event with external ID: " + eventExternalId + " not found"));
 
+    public TicketTypeResponse createTicketType(String eventExternalId, CreateTicketTypeRequest createTicketTypeRequest) {
         return null;
     }
 }
