ae1cb3938e6d73fa9c7c17e8f713cc5cb55c78c9
d-kens <omondikens1@gmail.com>
2026-07-14 07:57:04 +0300

    feature(add-ticket-type): Setup signal based forms for ticket type form

--- web/src/app/features/events/add-edit-ticket-type/add-edit-ticket-type.ts
+++ web/src/app/features/events/add-edit-ticket-type/add-edit-ticket-type.ts
@@ -5,6 +5,17 @@ import { EventsService } from '@features/events/services/events-service';
 import { SnackbarService } from '@shared/services/snackbar-service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { EventHero } from '../components/event-hero/event-hero';
+import { form, min, required } from '@angular/forms/signals';
+
+export interface TicketTypeFormData {
+  name: string;
+  price: number;
+  quantity: number;
+  description: string;
+  salesStartDate: string;
+  salesEndDate: string;
+  maxPerOrder: number;
+}
 
 @Component({
   selector: 'app-add-edit-ticket-type',
@@ -17,6 +28,25 @@ export class AddEditTicketType implements OnInit {
   private route = inject(ActivatedRoute);
   private eventService = inject(EventsService);
   private snackbarService = inject(SnackbarService);
+
+  ticketType = signal<TicketTypeFormData>({
+    name: '',
+    price: 1,
+    quantity: 0,
+    description: '',
+    salesStartDate: '',
+    salesEndDate: '',
+    maxPerOrder: 0,
+  });
+
+  ticketTypeForm = form(this.ticketType, (fields) => {
+    required(fields.name);
+    required(fields.price);
+    required(fields.quantity);
+
+    min(fields.price, 1, { message: 'Price must be a minimum of KES 1' });
+    min(fields.quantity, 1, { message: 'Quantity must be a minimum of 1' });
+  });
 
   isLoading = signal(true);
   eventId = signal(this.route.snapshot.paramMap.get('eventId')!);
