7b2e13398ea3129d8caa175d9decf6016d1b7681
d-kens <omondikens1@gmail.com>
2026-09-01 14:16:37 +0300

    batman

--- api/src/main/java/com/entri/users/repository/UserRepository.java
+++ api/src/main/java/com/entri/users/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.entri.users.repository;
 
 import com.entri.users.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +10,8 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-    Optional<User> findByEmail(String email);
-    Optional<User> findByExternalKey(String externalKey);
+    boolean existsByEmailAndDeletedFalse(String email);
+    Optional<User> findByEmailAndDeletedFalse(String email);
+    Optional<User> findByExternalKeyAndDeletedFalse(String externalKey);
+    Page<User> findAllByDeletedFalse(Pageable pageable);
 }
